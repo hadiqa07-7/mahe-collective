62100238d140aa36c6cbe8d4a48ea650d5902efa
hadiqa07-7 <hadiqafatimaa07@gmail.com>
2026-05-28 22:45:22 +0500

    Improve mobile responsiveness

--- obj/Debug/net8.0/Mahe-Clothingwebsite.AssemblyInfo.cs
+++ obj/Debug/net8.0/Mahe-Clothingwebsite.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Mahe-Clothingwebsite")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a34a00aa78da717b56379e5fab4e83e33c8b430c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+aa54afb23fc4af81c200d2988390133d571f23ad")]
 [assembly: System.Reflection.AssemblyProductAttribute("Mahe-Clothingwebsite")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Mahe-Clothingwebsite")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
